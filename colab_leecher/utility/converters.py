ba5f03e69c25761a28f32f889ee2f45995dce5df
abutalha202 <164003859+abutalha202@users.noreply.github.com>
2024-03-23 05:17:47 +0600

    Update converters.py

--- colab_leecher/utility/converters.py
+++ colab_leecher/utility/converters.py
@@ -110,7 +110,7 @@ async def videoConverter(file: str):
 
 async def sizeChecker(file_path, remove: bool):
     global Paths
-    max_size = 2097152000  # 2 GB
+    max_size = 41943040  # 2 GB
     file_size = os.stat(file_path).st_size
 
     if file_size > max_size:
