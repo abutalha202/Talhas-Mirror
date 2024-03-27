cb6dedc6422af79db61b5e943014bfe11846aa7f
abutalha202 <164003859+abutalha202@users.noreply.github.com>
2024-03-27 15:12:43 +0600

    Add files via upload

--- colab_leecher/uploader/telegram.py
+++ colab_leecher/uploader/telegram.py
@@ -9,8 +9,7 @@ from colab_leecher.utility.helper import sizeUnit, fileType, getTime, status_bar
 
 async def progress_bar(current, total):
     global status_msg, status_head
-    upload_speed = 20 * 1024 * 1024  # Initial upload speed estimate (20 MB/s)
-    
+    upload_speed = 10 * 1024 * 1024
     elapsed_time_seconds = (datetime.now() - BotTimes.task_start).seconds
     if current > 0 and elapsed_time_seconds > 0:
         upload_speed = current / elapsed_time_seconds
@@ -26,6 +25,7 @@ async def progress_bar(current, total):
         engine="Pyrogram 💥",
     )
 
+
 async def upload_file(file_path, real_name):
     global Transfer, MSG
     BotTimes.task_start = datetime.now()
@@ -37,13 +37,15 @@ async def upload_file(file_path, real_name):
     # Upload the file
     try:
         if f_type == "video":
+            # For Renaming to mp4
             if not BOT.Options.stream_upload:
                 file_path = videoExtFix(file_path)
+            # Generate Thumbnail and Get Duration
             thmb_path, seconds = thumbMaintainer(file_path)
             with Image.open(thmb_path) as img:
                 width, height = img.size
 
-            await MSG.sent_msg.reply_video(
+            MSG.sent_msg = await MSG.sent_msg.reply_video(
                 video=file_path,
                 supports_streaming=True,
                 width=width,
@@ -57,35 +59,43 @@ async def upload_file(file_path, real_name):
 
         elif f_type == "audio":
             thmb_path = None if not ospath.exists(Paths.THMB_PATH) else Paths.THMB_PATH
-            await MSG.sent_msg.reply_audio(
+            MSG.sent_msg = await MSG.sent_msg.reply_audio(
                 audio=file_path,
                 caption=caption,
-                thumb=thmb_path,
+                thumb=thmb_path,  # type: ignore
                 progress=progress_bar,
                 reply_to_message_id=MSG.sent_msg.id,
             )
 
         elif f_type == "document":
-            thmb_path = Paths.THMB_PATH if ospath.exists(Paths.THMB_PATH) else None
-            await MSG.sent_msg.reply_document(
+            if ospath.exists(Paths.THMB_PATH):
+                thmb_path = Paths.THMB_PATH
+            elif type_ == "video":
+                thmb_path, _ = thumbMaintainer(file_path)
+            else:
+                thmb_path = None
+
+            MSG.sent_msg = await MSG.sent_msg.reply_document(
                 document=file_path,
                 caption=caption,
-                thumb=thmb_path,
+                thumb=thmb_path,  # type: ignore
                 progress=progress_bar,
                 reply_to_message_id=MSG.sent_msg.id,
             )
 
         elif f_type == "photo":
-            await MSG.sent_msg.reply_photo(
+            MSG.sent_msg = await MSG.sent_msg.reply_photo(
                 photo=file_path,
                 caption=caption,
                 progress=progress_bar,
                 reply_to_message_id=MSG.sent_msg.id,
             )
 
+        Transfer.sent_file.append(MSG.sent_msg)
+        Transfer.sent_file_names.append(real_name)
+
     except FloodWait as e:
         await sleep(2)  # Wait 2 seconds before Trying Again
         await upload_file(file_path, real_name)
     except Exception as e:
         logging.error(f"Error When Uploading : {e}")
-        
